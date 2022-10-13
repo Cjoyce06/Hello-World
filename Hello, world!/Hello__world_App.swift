e844afddc5dd672fe4bd0b952d550718367d598f
Colin Joyce <Cjoyce1@bsd220.org>
2022-10-13 09:51:45 -0500

    Initial Commit

---
+++ Hello, world!/Hello__world_App.swift
@@ -0,0 +1,17 @@
+//
+//  Hello__world_App.swift
+//  Hello, world!
+//
+//  Created by Colin Joyce on 9/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Hello__world_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Hello, world!UITests/Hello__world_UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Hello__world_UITestsLaunchTests.swift
+//  Hello, world!UITests
+//
+//  Created by Colin Joyce on 9/3/22.
+//
+
+import XCTest
+
+class Hello__world_UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
